b3380803bf372ea3f84a9827c4a983df9aaa5717
Laurent Huot <huot.laurent11@gmail.com>
2018-07-08 13:44:50 -0400

    login fonctionnel

--- src/App.jsx
+++ src/App.jsx
@@ -107,7 +107,7 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<h1>This is the main App component</h1>
+				<h1>FDC Composites</h1>
 				<Header user={this.state.user} />
 				{/* LINKS to our different 'pages' */}
 				<DisplayLinks _logout={this._logout} loggedIn={this.state.loggedIn} />
